6391258f37a2aeda7f69a1a9e67a4042b5010b1f
Zaid Ilyas <81157594+zaidilyas89@users.noreply.github.com>
2024-08-21 11:43:32 +0800

    Add files via upload

---
+++ models/vertenet.py
@@ -0,0 +1,23 @@
+from .dec_net import DecNet_vertenet
+from . import resnet
+import torch.nn as nn
+import numpy as np
+import timm_local
+class vertenet(nn.Module):
+    def __init__(self, heads, pretrained, down_ratio, final_kernel, head_conv):
+        super(vertenet, self).__init__()
+        assert down_ratio in [2, 4, 8, 16]
+        # channels = [3, 64, 64, 128, 256, 512]
+        channels = [3, 24, 48, 64, 160, 256]
+        self.l1 = int(np.log2(down_ratio))
+        # self.base_network = resnet.resnet34(pretrained=pretrained)
+        self.base_network = timm_local.create_model('tf_efficientnetv2_s', pretrained=True, features_only=True)
+        self.dec_net = DecNet_vertenet(heads, final_kernel, head_conv, channels[self.l1])
+
+
+    def forward(self, x):
+        x1 = x
+        x = self.base_network(x)
+        x.insert(0, x1)
+        dec_dict = self.dec_net(x)
+        return dec_dict
